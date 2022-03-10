c7535f3fe28cb158b003da34d677497b6b6c0507
lakshmisunil-menon <L00163090@student.lyit.ie>
2022-03-10 12:30:45 +0000

    R practical

---
+++ TEST1.R
@@ -0,0 +1,25 @@
+1:20
+pi:10
+
+15:1
+seq(1, 20)
+seq(0, 10, 0.5)
+seq(5, 10 , length=30)
+my_seq <- seq(5, 10 , length=30)
+length (my_seq)
+rep(0, times = 40)
+vector <- c(0, 1, 2)
+rep(c(0, 1, 2), each = 10)
+num_vector <- c(0.5, 55, -10, 6)
+num_vector
+tf <- num_vector < 1
+tf
+num_vector >=6
+my_char <- c("My", "Name", "is")
+my_char
+paste("My", "Name", "is", sep =" ")
+my_line <- paste(my_char, collapse = " ")
+my_line
+my_name <- paste(c(my_line, "Lakshmi"), collapse = " ")
+my_name
+
